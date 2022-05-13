663430b1f4b6725d87107460e2bd0ad30b3385f3
KJLuebeck <Kathi-Jaeger@t-online.de>
2022-05-13 15:35:39 +0200

    Exercise01 print

---
+++ Exercise01.py
@@ -0,0 +1,6 @@
+print ("I am Katharina Jaeger.")
+#gibt den Satz aus
+print ("I was born on 28. May 1985.")
+print ("I am a participant of this course called bioinformatics and biostatistics at cq.")
+print ("Today is friday @}-,-`-.")
+print ("STOP!!!")
